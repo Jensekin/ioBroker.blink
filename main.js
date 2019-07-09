a3837f148cfa142e5bced1090f4a74065c5b5b93
root <root@ioBroker-Rock64.fritz.box>
2019-07-09 20:06:16 +0200

    cleanup logs, hide password

--- main.js
+++ main.js
@@ -23,19 +23,16 @@ class Blink extends utils.Adapter {
             name: 'blink',
         });
         this.on('ready', this.onReady.bind(this));
-        this.on('objectChange', this.onObjectChange.bind(this));
         this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
-        this.on('unload', this.onUnload.bind(this));
     }
 
     createStateObjectsFromSummary(summary){
-	this.log.info("start creating objects");
+	this.log.debug("start creating objects");
 	const promises = [];
 	Object.entries(summary.network).forEach( (networkAttr) => {
 	    var key = networkAttr[0];
 	    var val = networkAttr[1];
-	    this.log.info("creating network object "+summary.network.name+'.'+key);
+	    this.log.debug("creating network object "+summary.network.name+'.'+key);
 	    promises.push(this.setObjectNotExistsAsync(summary.network.name+'.'+key, {
                 type: 'state',
                 common: {
@@ -88,18 +85,18 @@ class Blink extends utils.Adapter {
     }
 
     pollStatusFromBlinkServers(scope, intsecs){
-	scope.log.info("start polling from server. interval " + intsecs + " seconds.");
+	scope.log.debug("start polling from server. interval " + intsecs + " seconds.");
 	scope.blinkapi.setupSystem().then(() => {
-	    scope.log.info("connection set up");
+	    scope.log.debug("connection set up");
             scope.blinkapi.getSummary().then((summary) => {
-		scope.log.info("processing summary");
+		scope.log.debug("processing summary");
 		let promises = scope.createStateObjectsFromSummary(summary);
 		Promise.all(promises).then(() => {
-                    scope.log.info("update states from summary");
+                    scope.log.debug("update states from summary");
 		    scope.updateStatesFromSummary(summary);
-	            scope.log.info("updated states, setting timer in "+intsecs+" seconds");
+	            scope.log.debug("updated states, setting timer in "+intsecs+" seconds");
 	            setTimeout(scope.pollStatusFromBlinkServers, intsecs * 1000, scope, intsecs);
-		    scope.log.info("timer set, all is done");
+		    scope.log.debug("timer set, all is done");
 	        }).catch((err) => {
 		    scope.log.error("error: " + err);
 		    setTimeout(scope.pollStatusFromBlinkServers, intsecs * 1000, scope, intsecs);
@@ -119,10 +116,10 @@ class Blink extends utils.Adapter {
 
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
-        this.log.info('config URL: ' + this.config.url);
-        this.log.info('config Username: ' + this.config.username);
-        this.log.info('config Password: ' + this.config.password);
-	this.log.info('config Interval: ' + this.config.interval);
+        this.log.debug('config URL: ' + this.config.url);
+        this.log.debug('config Username: ' + this.config.username);
+        this.log.debug('config Password: ' + this.config.password);
+	this.log.debug('config Interval: ' + this.config.interval);
 
 	this.blinkapi = new blinkclass(this.config.username, this.config.password);
 	this._authtoken = '';
@@ -130,34 +127,6 @@ class Blink extends utils.Adapter {
         // in this template all states changes inside the adapters namespace are subscribed
         this.subscribeStates('*');
         this.pollStatusFromBlinkServers(this, this.config.interval);
-    }
-
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
-    onUnload(callback) {
-        try {
-            this.log.info('cleaned everything up...');
-            callback();
-        } catch (e) {
-            callback();
-        }
-    }
-
-    /**
-     * Is called if a subscribed object changes
-     * @param {string} id
-     * @param {ioBroker.Object | null | undefined} obj
-     */
-    onObjectChange(id, obj) {
-        if (obj) {
-            // The object was changed
-            this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-        } else {
-            // The object was deleted
-            this.log.info(`object ${id} deleted`);
-        }
     }
 
     /**
@@ -171,7 +140,7 @@ class Blink extends utils.Adapter {
             if(state.ack === true){
                 return;
 	    }
-	    this.log.info(`state ${id} was changed from outside to: ${state.val}`);
+	    this.log.debug(`state ${id} was changed from outside to: ${state.val}`);
 	    var idsplit = id.split('.');
 	    var statename = idsplit[idsplit.length-1];
 	    if(idsplit.length == 4 && statename == 'armed'){
@@ -193,23 +162,6 @@ class Blink extends utils.Adapter {
             this.log.info(`state ${id} deleted`);
         }
     }
-
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.message" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    // 	if (typeof obj === 'object' && obj.message) {
-    // 		if (obj.command === 'send') {
-    // 			// e.g. send email or pushover or whatever
-    // 			this.log.info('send command');
-
-    // 			// Send response in callback if required
-    // 			if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    // 		}
-    // 	}
-    // }
 
 }
 
